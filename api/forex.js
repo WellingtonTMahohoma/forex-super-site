65f63dd2255852c0a08aba2c98d28e23bc395359
WellingtonTMahohoma <wellingtontmahohoma@gmail.com>
2026-08-25 19:01:12 +0530

    Update forex.js

--- api/forex.js
+++ api/forex.js
@@ -1,7 +1,9 @@
 export default async function handler(req, res) {
+  const { from = "EUR", to = "USD" } = req.query; // defaults
   const alphaKey = process.env.ALPHA_VANTAGE_KEY;
+
   const response = await fetch(
-    `https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol=EUR&to_symbol=USD&interval=5min&apikey=${alphaKey}`
+    `https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol=${from}&to_symbol=${to}&interval=5min&apikey=${alphaKey}`
   );
   const data = await response.json();
   res.status(200).json(data);
